bdb638c5f51ffa345bfceac9ff961b0b5d583d80
Saral <saral.guptax@gmail.com>
2024-03-03 00:57:25 +0530

    Thir Commit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,3 @@
-import { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import CanvasLoader from "../Loader";
-
 const Computers = () => {
   const computer = useGLTF("•/desktop_pc/scene. gltf");
   return (
