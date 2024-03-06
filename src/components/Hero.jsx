f5af4e52ab4f15651aca8fb57839a247b42b9487
Saral <saral.guptax@gmail.com>
2024-03-06 17:45:58 +0530

    New Changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,27 +45,27 @@ const Hero = () => {
           </motion.p>
           <motion.div
             variants={textVariant(1)}
-            className="relative w-full flex justify-center items-center mt-20 "
+            className="relative w-full flex sm:flex-row sm:justify-center items-center flex-col gap-4 mt-20 "
           >
             <a href="#about">
-              <button className="flex justify-evenly sm:w-[12rem] w-[10rem] mr-5 shadow-card hover:bg-white hover:text-black  hover:border-[#4d4dff] transition-all text-white font-grotesk font-bold sm:text-[24px] text-[20px] bg-[#4d4dff] px-4 py-3 rounded-md group">
+              <button className="flex justify-evenly items-center sm:w-[12rem] w-[9rem] shadow-card hover:bg-white hover:text-black  hover:border-[#4d4dff] transition-all text-white font-grotesk font-bold sm:text-[24px] text-[18px] bg-[#4d4dff] px-4 py-3 rounded-md group">
                 <img
                   src={ar}
                   alt="github"
-                  className="w-9 h-9 bg-white p-1 rounded-full "
+                  className="sm:w-9 sm:h-9 h-8 w-8 bg-white p-1 rounded-full "
                 />
                 Explore
               </button>
             </a>
             <a href="https://github.com/saral-gupta7" target="_blank">
               <button
-                className="flex items-center justify-evenly sm:w-[12rem] w-[10rem] text-[#000] font-grotesk font-bold sm:text-[24px] text-[20px]
+                className="flex items-center justify-evenly sm:w-[12rem] w-[9rem] text-[#000] font-grotesk font-bold sm:text-[24px] text-[18px]
                bg-white px-4 py-3 rounded-md hover:scale-[1.01] shadow-card transition transform hover:bg-black hover:text-white"
               >
                 <img
                   src={git}
                   alt="github"
-                  className="w-9 h-9 bg-black rounded-full "
+                  className="sm:w-9  sm:h-9 h-8 w-8 bg-black rounded-full "
                 />
                 Github
               </button>
